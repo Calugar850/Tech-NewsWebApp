19fbd8eb810564f07ded3ccdcb9f994a7e493810
Calugar850 <72302669+Calugar850@users.noreply.github.com>
2021-12-16 21:57:19 +0200

    Security changes

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&+ud)7r49q%g6%f1d&&3_z#4j!csm@yp(%4+@1+5bg7j+-iu%j'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
